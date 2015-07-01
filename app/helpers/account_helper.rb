77267b27de07a7ef9fdb6de7ee566b5c0605f390
Sachin <sravich2@illinois.edu>
2015-07-01 22:36:39 +0530

    Display preview for webpage

--- app/helpers/account_helper.rb
+++ app/helpers/account_helper.rb
@@ -3,17 +3,27 @@ require 'open-uri'
 
 module AccountHelper
   def sandbox
-    open_calais = OpenCalais::Client.new
-    response = open_calais.enrich('Ruby on Rails is a fantastic web framework. It uses MVC, and the Ruby programming language invented by Matz in Japan.')
 
-# which has the 'raw' response
-    response.raw
+    source = open('http://www.theverge.com/2015/6/30/8870415/spaceX-explosion-NASA-America-space-future').read
 
-# and has been parsed a bit to get :language, :topics, :tags, :entities, :relations, :locations
-# as lists of hashes
-    response.tags.each{|t| puts t[:name] }
-    # source = open('http://www.theverge.com/2015/7/1/8875969/microsoft-surface-pro-3-128gb-i7-processor').read
-    # content = Readability::Document.new(source).content
-    # sanitize(content, :tags => [])
+    # Preview display of web page (DONT REMOVE THIS BLOCK)
+    # content = Readability::Document.new(source)
+    # content.prepare_candidates
+    # content.html.to_s.html_safe
+
+    content = Readability::Document.new(source, :tags => %w[div p]).content
+    strip_tags(content).to_s.html_safe
+#     open_calais = OpenCalais::Client.new
+#
+#     response = open_calais.enrich(content)
+#
+# # which has the 'raw' response
+# #     response.raw
+#
+# # and has been parsed a bit to get :language, :topics, :tags, :entities, :relations, :locations
+# # as lists of hashes
+# #     response.topics.each{|t| puts t[:name]}
+#     response.tags.inspect
+
   end
 end
