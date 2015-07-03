bf84608911b9f69b8fc67c0fc55887bec053d74d
Sachin <sravich2@illinois.edu>
2015-07-03 11:47:54 +0530

    GetContentTagsJob v1

--- app/jobs/get_content_and_tags_job.rb
+++ app/jobs/get_content_and_tags_job.rb
@@ -1,7 +1,18 @@
+require 'open-uri'
+
 class GetContentAndTagsJob < ActiveJob::Base
   queue_as :default
 
   def perform(*args)
-    puts 'Works as expected'
+    current_article = Article.where(:id => args.at(0)).first
+    source = open(current_article.url).read
+    content = Readability::Document.new(source, :tags => %w[div p]).content
+    content = ActionView::Base.full_sanitizer.sanitize(content)
+    current_article.content = content
+
+    open_calais_response = OpenCalaisClient.client.enrich(content)
+    current_article.tags = open_calais_response.tags
+    current_article.topics = open_calais_response.topics
+    current_article.save
   end
 end
