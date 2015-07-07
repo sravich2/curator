cf9e6308051a301f53c9ead553a871d3d5329558
Sachin <sravich2@illinois.edu>
2015-07-07 19:45:44 +0530

    Added back chart data

--- app/helpers/account_helper.rb
+++ app/helpers/account_helper.rb
@@ -1,4 +1,17 @@
 module AccountHelper
   def sandbox
+    hash = Hash.new
+    Article.all.each do |a|
+      a.topics.each do |k, v|
+        if v.to_f > 0.8
+          if hash[k].nil?
+            hash[k] = 1
+          else
+            hash[k] += 1
+          end
+        end
+      end
+    end
+    hash.sort_by { |_, v| -v }[0..30].to_h
   end
 end
