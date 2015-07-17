3a15fad2a0a34f9bf012b62424cdaf72c47c74a0
Sachin <sravich2@illinois.edu>
2015-07-17 11:31:30 +0530

    Slight UI change

--- app/helpers/account_helper.rb
+++ app/helpers/account_helper.rb
@@ -1,18 +1,4 @@
 module AccountHelper
   def sandbox
-    client = OpenCalaisClient.client
-    entities = Hash.new
-    client.enrich(Article.all[3].content).entities.each do |entity_info|
-      name = entity_info[:name]
-      type = entity_info[:type]
-      score = entity_info[:score]
-      if entities[type].nil?
-        entities[type] = Hash(name => score)
-      else
-        entities[type][name] = score
-      end
-    end
-    y = entities.to_yaml
-    YAML::load(y)
   end
 end
