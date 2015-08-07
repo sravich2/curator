0e30ee2cb4c010fb63b267c1200a502f22e6194a
Sachin <sravich2@illinois.edu>
2015-08-07 17:33:03 +0530

    Liked tags when clicked

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -4,6 +4,22 @@ class LikesController < ApplicationController
     u_id = params[:user_id]
     unless Like.where(:article_id => a_id).where(:user_id => u_id).exists?
       Like.create(:user_id => u_id, :article_id => a_id)
+      current_article = Article.find(a_id)
+      current_user = User.first
+
+      attrs = ['tags', 'topics', 'entities']
+
+      attrs.each do |attr|
+        current_article.most_likely(attr.to_sym).each do |a|
+          current_value = current_user.liked_tags[attr][a]
+          if current_value.nil?
+            current_user.liked_tags[attr][a] = 1
+          else
+            current_user.liked_tags[attr][a] = current_value + 1
+          end
+        end
+      end
+      current_user.save
     end
     render :nothing => true
   end
