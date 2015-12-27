22c5e83c75ce548150739170f88bb92491ca83b3
Sachin <sravich2@illinois.edu>
2015-12-26 21:36:57 -0600

    Share button implemented

--- app/assets/javascripts/article-show.js
+++ app/assets/javascripts/article-show.js
@@ -8,8 +8,9 @@ $(document).ready(function() {
     });
 
     $('.length-settings').on('click', '*', function() {
+        var article = $('.article');
         var action = $(this).attr('length-action');
-        var currentSetting = pxToPercent($('.article'));
+        var currentSetting = pxToPercent(article);
         var newSetting;
         if (action == '+') {
             newSetting = currentSetting + 10 + '%';
@@ -19,13 +20,14 @@ $(document).ready(function() {
         }
         if (parseInt(newSetting) <= 100 && parseInt(newSetting)  >= 30) {
             var extend_css_class = {'width': newSetting};
-            $('.article').css(extend_css_class);
+            article.css(extend_css_class);
         }
     });
 
     $('.font-size-settings').on('click', '*', function() {
+        var paragraphs = $('p');
         var action = $(this).attr('font-size-action');
-        var currentSetting = $('p').css('font-size');
+        var currentSetting = paragraphs.css('font-size');
         var newSetting;
         if (action == '+') {
             newSetting = (parseInt(currentSetting) + 1) + 'px';
@@ -33,15 +35,15 @@ $(document).ready(function() {
         else if (action == '-') {
             newSetting = (parseInt(currentSetting) - 1) + 'px';
         }
-        var font_size_css_class = {'font-size': newSetting};
-        $('p').attr('style', 'font-size: '.concat(newSetting).concat(' !important') );
+        //var font_size_css_class = {'font-size': newSetting};
+        paragraphs.attr('style', 'font-size: '.concat(newSetting).concat(' !important') );
     });
 
     $('.background-settings').on('click', '*', function() {
         var newBgColor = $(this).attr('bg-color-value');
         var bg_color_css_class;
         if (newBgColor != 'black') {
-            bg_color_css_class = {'background-color': newBgColor, color: 'black'};
+            bg_color_css_class = {'background': newBgColor, color: 'black'};
         }
         else {
             bg_color_css_class = {'background-color': newBgColor, color: 'white'}
@@ -59,4 +61,18 @@ $(document).ready(function() {
     var pxToPercent = function(element) {
         return Math.round( 100 * parseFloat(element.css('width')) / parseFloat(element.parent().css('width')) ); // Converts px to %
     }
+});
+
+$(document).ready(function() {
+
+    config = {
+        url: $('#url').data('url'),
+        ui: {
+            flyout: 'middle left'
+        }
+    };
+
+    var share = new ShareButton('.share-button', config);
+    window.alert(share.config.url);
+    //window.alert($('#url').data('url'));
 });
